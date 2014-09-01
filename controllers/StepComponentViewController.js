388a6f40911baaa10eceaa8f2106f6ed844050ab
Elizabeth Levosinski <levosins@gmail.com>
2014-09-01 18:44:18 -0400

    no message

--- controllers/StepComponentViewController.js
+++ controllers/StepComponentViewController.js
@@ -1,6 +1,11 @@
 app.controller('StepComponentViewController', ['$scope', '$http', function($scope, $http) {
 	$http.get('../data/data_dinosaurs.json').success(function(data) {
-    	console.log("success");
         $scope.dinosaurs = data.dinosaurs;
+        $scope.defaultSpecies = $scope.dinosaurs[0].species;
+        $scope.defaultCountry = $scope.dinosaurs[0].country[0];
+        $scope.defaultPeriod = $scope.dinosaurs[0].time_period;
+        $scope.defaultBody = $scope.dinosaurs[0].body_type;
     });  
+
+
 }]);

1b72723d733ad6147d9ff837de825c73d3076a81
Liz <llevosinski@enlighten.com>
2014-09-08 11:42:23 -0400

    trying to fix gulp error using autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
  var gulp = require('gulp'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    prefix = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
    gulp.src('scss/*.scss')
       .pipe(watch())
       .pipe(sass())
+      .pipe(prefix('last 2 versions', {map: false }))
       .pipe(gulp.dest('css'))
       .pipe(livereload());
 });
